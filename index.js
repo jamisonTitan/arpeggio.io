78c5ff96785f10407d1003218d6ad05d6bde393c
Titan <jamisontitan@gmail.com>
2018-11-24 12:43:58 -0800

    made some changes with how it reads chords really shoddy right now

--- index.js
+++ index.js
@@ -1,64 +1,10 @@
 const chromatic = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
-for(note in chromatic){chromatic.push(chromatic[note])} //mkae chromatic loop
+for(note in chromatic){chromatic.push(chromatic[note])}//make chromatic loop
 const whiteKeys = [];
 const blackKeys = [];
 const width = 800, height = 500;
-let curentNotes = [];
+let currentNotes = [];
 const scale = 12;
-
-
-const blackKey = (note, startx) => {
-	return {
-		show() {
-			if(!currentNotes.includes(note)) {
-				fill(0);
-				stroke(0);
-				strokeWeight(2);
-				rect(startx, height / 2 - 2, scale * (2/3), 32);
-			}else {
-				fill(200,100,100);
-				stroke(0);
-				strokeWeight(2);
-				rect(startx, height / 2 - 2, scale * (2/3), 32);
-			}
-		}	
-	}
-}
-
-const whiteKey = (note, startx) => {
-	return {
-		show() {
-			if(!currentNotes.includes(note)) {
-				fill(255);
-				stroke(0);
-				strokeWeight(2);
-				rect(startx, height / 2, scale, 48);
-			}else {
-				fill(200,100,100);
-				stroke(0);
-				strokeWeight(2);
-				rect(startx, height / 2, scale, 48);
-			}
-		}	
-	}
-}
-
-const fillKeys = () => {
-	let tempx = 50;
-	for(let i = 0; i < 7; i++){
-		for(let i = 0; i < 12; i++) {
-			if( !chromatic[i].includes('#') ) {
-				whiteKeys.push(whiteKey( chromatic[i], tempx ));
-				tempx += scale;
-			}else {
-				//TODO sharps
-				tempx -= scale / 4;
-				blackKeys.push(blackKey( chromatic[i], tempx ));
-				tempx += scale / 4;
-			}
-		}
-	}
-}
 
 function setup() {
 	let canvas = createCanvas(width, height);
@@ -68,7 +14,7 @@ function setup() {
 }
 
 function draw() {
-	background(100,100,200, 0.5);
+	background(100,100,200);
 	for(key of whiteKeys){key.show()}
 	for(key of blackKeys){key.show()}
 }
@@ -119,8 +65,122 @@ const createMinorScale = tonic => {
 	halfStep(); 
 	push(); //minor sixth
 	wholeStep();
-	push(); //major seventh
+	push(); //minor seventh
 	return result;
 }
-//uncomment later
-//console.log(chromatic.map(note => createMajorScale(note)));
+
+const createChord = (tonic, type, extensions) => {
+	console.log(extensions);
+	if(!chromatic.includes(tonic)){
+		//TODO error handling
+	}
+	let result = [];
+	let scale;
+	if(type === 'major'){
+		scale = createMajorScale(tonic.toUpperCase());
+	}else if (type === 'minor') {
+		scale = createMinorScale(tonic.toUpperCase());
+	}else {
+		scale = createMajorScale(tonic.toUpperCase());
+	}
+	for(note in scale){scale.push(scale[note])}//make scale loop
+	console.log(scale);
+	result.push(scale[0] + '4');
+	result.push(scale[2] + '4');
+	result.push(scale[4] + '4');
+	if(extensions){
+	extensions.forEach(extension => {
+			console.log(extension);
+			if(extension < 8){
+				result.push(scale[extension - 1] + '4');
+			}else {
+				result.push(scale[extension - 1] + '5');
+			}
+		});
+	}
+	return result;
+}
+
+const blackKey = (note_, startx, highlightable) => {
+	let note = note_;
+	return {
+		get note() {
+			return note;
+		},
+		show() {
+			if(!currentNotes.includes(note)) {
+				fill(0);
+				stroke(0);
+				strokeWeight(2);
+				rect(startx, height / 2 - 2, scale * (2/3), 32);
+			}else {
+				fill(200,100,100);
+				stroke(0);
+				strokeWeight(2);
+				rect(startx, height / 2 - 2, scale * (2/3), 32);
+			}
+		}	
+	}
+}
+
+const whiteKey = (note, startx) => {
+	return {
+		get note() {
+			return note;
+		},
+		show() {
+			if(!currentNotes.includes(note)) {
+				fill(255);
+				stroke(0);
+				strokeWeight(2);
+				rect(startx, height / 2, scale, 48);
+			}else {
+				fill(200,100,100);
+				stroke(0);
+				strokeWeight(2);
+				rect(startx, height / 2, scale, 48);
+			}
+		}	
+	}
+}
+
+const fillKeys = () => {
+	let tempx = 50 - scale;
+	for(let i = 0; i < 7; i++){
+		for(let j = 0; j < 12; j++) {
+			if( !chromatic[j].includes('#') ) {
+				whiteKeys.push(whiteKey( chromatic[j] + (i).toString(), tempx));
+				tempx += scale;
+			}else {
+				tempx -= scale / 4;
+				blackKeys.push(blackKey( chromatic[j] + (i).toString(), tempx));
+				tempx += scale / 4;
+			}
+		}
+	}
+}
+
+$(document).ready(function(){
+	$('#btn').on('click', function() {
+		let  str = $('#in').val();
+		console.log(str);
+		str = str.split(' ');
+		console.log(str);
+		switch(str.length) {
+			case 1:
+				currentNotes = createChord(str[0].toUpperCase(), 'major');
+			break;
+			case 2:
+				currentNotes = createChord(str[0].toUpperCase(), str[1].toLowerCase());
+			break;
+			default:
+				let ext = str.splice(2, str.length - 2).map(ext => parseInt(ext, 10));
+				console.log(ext);
+				currentNotes = 
+					createChord(str[0].toUpperCase(), str[1].toLowerCase(), ext);
+			break;
+		}
+		console.log(currentNotes);
+	});
+});
+
